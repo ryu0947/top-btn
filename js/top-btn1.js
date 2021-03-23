05c2fa46a10c570d12a350f4a3f5fa0444542623
ryu <dragonogawaz@gmail.com>
2021-03-23 10:05:35 +0900

    TOPへ戻る動作の実装

--- js/top-btn1.js
+++ js/top-btn1.js
@@ -1,2 +1,10 @@
 "use strict";
 
+const topBtn = document.getElementById("js-top-btn");
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
